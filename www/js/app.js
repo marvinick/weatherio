eb54a79a6887655e6197555024eddb0d04c3ef0e
Marvki <marvkipi@gmail.com>
2016-04-14 00:03:38 -0700

    locations service

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,43 @@ angular.module('App', ['ionic'])
 	return Settings;
 })
 
+.factory('Locations', function()  {
+	var Locations = {
+		data: [{
+			city: 'Chicago, IL, USA',
+			lat: 41.8781136,
+			lng: -87.6297982
+		}],
+		getIndex: function (item) {
+			var Index = -1;
+			angular.forEach(Locations.data, function (location, i) {
+				if (item.lat == location.lat && item.lng == location.lng) {
+					index = i;
+				}
+			});
+			return index;
+		}, 
+		toggle: function (item) {
+			var index = Locations.getIndex(item);
+			if (index >= 0) {
+				Locations.data.splice(index, 1);
+			} else {
+				Locations.data.push(item);
+			}
+		},
+		primary: function (item) {
+			var index = Locations.getIndex(item);
+			if (index >= 0) {
+				Locations.data.splice(index, 1);
+				Locations.data.splice(0,0,item);
+			} else {
+				Locations.data.unshift(item);
+			}
+		}
+	};
+	return Locations;
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -27,4 +64,4 @@ angular.module('App', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
