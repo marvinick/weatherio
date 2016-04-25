3476b571c0cfd8c782e993305cbed9429e504985
Marvki <marvkipi@gmail.com>
2016-04-24 20:35:36 -0700

    filters for forecast data

--- www/js/app.js
+++ www/js/app.js
@@ -71,6 +71,42 @@ angular.module('App', ['ionic'])
 	return Locations;
 })
 
+.filter('timezone', function() {
+	return function (input, timezone) {
+		if (input && timezone) {
+			var time = moment.tz(input * 1000, timezone);
+			return time.format('LT');
+		}
+		return '';
+	};
+}) 
+.filter('chance', function() {
+	return function (chance) {
+		if (chance) {
+			var value = Math.round(chance * 10);
+			return value * 10;
+		}
+		return 0;
+	};
+})
+.filter('icons', function() {
+	var map = {
+		'clear-day': 'ion ios-sunny',
+		'clear-night': 'ion ios-moon',
+		rain: 'ion-ios-rainy',
+		snow: 'ion-ios-snowy',
+		sleet: 'ion-ios-rainy',
+		wind: 'ion-ios-flag',
+		fog: 'ion-ios-cloud',
+		cloudy: 'ion-ios-cloudy',
+		'partly-cloudy-day': 'ion-ios-partlysunny',
+		'partly-cloudy-night': 'ion-ios-cloudy-night'
+	};
+	return function (icon) {
+		return map[icon] || '';
+	}
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
